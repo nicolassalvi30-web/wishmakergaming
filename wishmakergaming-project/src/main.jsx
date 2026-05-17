0b9596b59481543996202768cb0bb3c30a99b8a1
nicolassalvi30-web <nicolassalvi30@gmail.com>
2026-05-17 22:45:55 +0100

    Update main.jsx

--- wishmakergaming-project/src/main.jsx
+++ wishmakergaming-project/src/main.jsx
@@ -391,7 +391,7 @@ function ReviewPage({ slug }) {
             )}
 
   <div className="articleBody ultimateBody">
-  {post.body.split('\n').map((line, i) => {
+ {articleBody.split('\n').map((line, i) => {
                 const clean = line.trim();
                 if (!clean) return <br key={i}/>;
                 if (clean.startsWith('# ')) return <h2 key={i}>{clean.replace('# ', '')}</h2>;
